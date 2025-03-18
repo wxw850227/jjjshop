04fd384b33624f83477fd9e7d2209c98a0b2a5ef
cookiemy <1946749843@qq.com>
2025-03-18 11:43:31 +0800

    初始化1

--- app/app/provider.php
+++
@@ -1,9 +0,0 @@
-<?php
-use app\common\exception\ExceptionHandler;
-use app\Request;
-
-// 容器Provider定义文件
-return [
-    'think\Request'          => Request::class,
-    'think\exception\Handle' => ExceptionHandler::class,
-];
--- uniapp/common/status.js
+++
@@ -1,9 +0,0 @@
-export default {
-   states:{
-        type:null,
-    },
-	
-    setStates(key,data){
-        this.states[key] = data;
-    }
-}
